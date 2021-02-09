a2297c2168f7b60ae4062869f50ca89668e5ce97
Patryk Polus <poluspatryk.deweloper@gmail.com>
2021-02-09 19:57:24 +0100

    Koniec zajec 09.02.2021

--- main.js
+++ main.js
@@ -59,12 +59,14 @@ const passengers = [
     name : "Beata",
     isDog :  false,
     sex : "women"   
-    },
+    }
 ]
 
         function displayPassenger() {
 
-            for(i=0; i <= passengers.length; i++) {
+            for(i=0; i < passengers.length; i++) {
+
+                
 
                 const gridPassengers  = document.getElementById("passengerGrid")
 
@@ -77,12 +79,18 @@ const passengers = [
                 h2Name.setAttribute("class", "namePassenger")              
                 paragrafSex.setAttribute("class", "passengerInfo")
                 paragrafIsDog.setAttribute("class", "passengerInfo")
-
+                
                 h2Name.innerText = "Imię: " + passengers[i].name
 
-                paragrafSex.innerText = "Płeć: " + passengers[i].sex
+                if(passengers[i].sex == "women") {
+                    sexPasseneger = "Kobieta" 
+                } else {
+                    sexPasseneger = "Mężczyzna" 
+                }
 
-                const isDog = passengers[i].isDog ? "Jest pies" : "Nie ma psa"
+                paragrafSex.innerText = "Płeć: " + sexPasseneger
+
+                const isDog = passengers[i].isDog ? "Ma psa" : "Nie ma psa"
 
                 paragrafIsDog.innerText = isDog
 
@@ -95,3 +103,80 @@ const passengers = [
             }
 
         }
+
+        function displayPost(){
+        fetch('https://jsonplaceholder.typicode.com/posts')
+        .then(res => res.json())
+        .then(res => {
+            
+
+            for(i=0; i < res.length; i++) {
+
+            const postsList = document.getElementsByClassName("postsList")
+               
+            const divPost = document.createElement("div")
+            const titlePost = document.createElement("h4")
+            const bodyPost = document.createElement("p")
+            const idPost = document.createElement("p")
+
+            divPost.setAttribute("class", 'divPost')
+            titlePost.setAttribute("class", 'titlePost')
+            idPost.setAttribute("class", 'idPost')
+            bodyPost.setAttribute("class", 'bodyPost')
+
+            titlePost.innerText = "Tytuł: " + res[i].title
+            idPost.innerText = "ID użytkownika: " + res[i].userId
+            bodyPost.innerHTML = res[i].body
+
+            divPost.append(titlePost)
+            divPost.append(idPost)
+            divPost.append(bodyPost)
+
+            postsList[0].append(divPost)
+
+            }
+
+        })
+    }
+
+    function displayImage(){
+        fetch('https://jsonplaceholder.typicode.com/photos')
+        .then(res => res.json())
+        .then(res => {
+            
+
+            for(i=0; i < res.length; i++) {
+
+                const imageGrid = document.getElementById("imageGrid")
+
+                const divCard = document.createElement("div")
+
+                const image = document.createElement("img")
+
+                const divCardBody = document.createElement("div")
+
+                const h5TitleImage = document.createElement("h5")
+
+                divCard.setAttribute("class", "card myCard")
+
+                image.setAttribute("class", "card-img-top")
+
+                image.src = res[i].thumbnailUrl
+
+                console.log(image)
+
+                divCardBody.setAttribute("class", "card-body")
+
+                h5TitleImage.setAttribute("class", "card-title")
+
+                h5TitleImage.innerText = res[i].title
+
+                divCardBody.append(h5TitleImage)
+
+                divCard.append(image)
+                divCard.append(divCardBody)
+
+                imageGrid.append(divCard)
+            }
+        })
+    }
